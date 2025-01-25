c0f8df953147d95cdb88a5b62da9c56aa12add08
alendavies <alendavies10@gmail.com>
2025-01-25 06:11:11 -0300

    dsadsa

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -3,7 +3,7 @@ import sharp from "sharp";
 import ico from "sharp-ico";
 import path from "node:path";
 
-const faviconSrc = path.resolve("/logo.png");
+const faviconSrc = path.resolve("public/logo.png");
 
 export const GET: APIRoute = async () => {
   // Resize the image to multiple sizes
