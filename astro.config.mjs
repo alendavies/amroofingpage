4a29445e6d736b8832c8ed77582515d239e75e0e
alendavies <alendavies10@gmail.com>
2025-01-25 03:53:41 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,30 +6,24 @@ import starlight from "@astrojs/starlight";
 
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
+import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
+
 export default defineConfig({
-  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://screwfast.uk",
+  site: "https://AMRoofing.com",
+
   image: {
     domains: ["images.unsplash.com"],
   },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "fr"],
-  //   fallback: {
-  //     fr: "en",
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   },
-  // },
+
   prefetch: true,
+
   integrations: [tailwind(), sitemap({
     i18n: {
-      defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
+      defaultLocale: "en",
       locales: {
         en: "en", // The `defaultLocale` value must present in `locales` keys
-        fr: "fr",
       },
     },
   }), starlight({
@@ -45,53 +39,21 @@ export default defineConfig({
         label: "English",
         lang: "en",
       },
-      de: { label: "Deutsch", lang: "de" },
-      es: { label: "Español", lang: "es" },
-      fa: { label: "Persian", lang: "fa", dir: "rtl" },
-      fr: { label: "Français", lang: "fr" },
-      ja: { label: "日本語", lang: "ja" },
-      "zh-cn": { label: "简体中文", lang: "zh-CN" },
     },
-    // https://starlight.astro.build/guides/sidebar/
     sidebar: [
       {
         label: "Quick Start Guides",
-        translations: {
-          de: "Schnellstartanleitungen",
-          es: "Guías de Inicio Rápido",
-          fa: "راهنمای شروع سریع",
-          fr: "Guides de Démarrage Rapide",
-          ja: "クイックスタートガイド",
-          "zh-cn": "快速入门指南",
-        },
         autogenerate: { directory: "guides" },
       },
-      {
-        label: "Tools & Equipment",
-        items: [
-          { label: "Tool Guides", link: "tools/tool-guides/" },
-          { label: "Equipment Care", link: "tools/equipment-care/" },
-        ],
-      },
-      {
-        label: "Construction Services",
-        autogenerate: { directory: "construction" },
-      },
-      {
-        label: "Advanced Topics",
-        autogenerate: { directory: "advanced" },
-      },
     ],
-    social: {
-      github: "https://github.com/mearashadowfax/ScrewFast",
-    },
     disable404Route: true,
     customCss: ["./src/assets/styles/starlight.css"],
-    favicon: "/favicon.ico",
+    favicon: "src/images/logo.png",
     components: {
       SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
       Head: "./src/components/ui/starlight/Head.astro",
-      MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
+      MobileMenuFooter:
+        "./src/components/ui/starlight/MobileMenuFooter.astro",
       ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
     },
     head: [
@@ -99,22 +61,25 @@ export default defineConfig({
         tag: "meta",
         attrs: {
           property: "og:image",
-          content: "https://screwfast.uk" + "/social.webp",
+          content: "https://AMRoofing.com" + "/social.webp",
         },
       },
       {
         tag: "meta",
         attrs: {
           property: "twitter:image",
-          content: "https://screwfast.uk" + "/social.webp",
+          content: "https://AMRoofing.com" + "/social.webp",
         },
       },
     ],
   }), compressor({
     gzip: false,
     brotli: true,
-  }), mdx()],
+  }), mdx(), icon()],
+
   experimental: {
     clientPrerender: true,
   },
+
+  adapter: netlify(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,42 +1,47 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors';
+import flyonui from "flyonui";
+import colors from "tailwindcss/colors";
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/preline/preline.js",
+    "./node_modules/flyonui/dist/js/*.js",
+    "../path/to/raty-js/**/*.js",
   ],
   darkMode: "class",
   theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('src/images/hero-image.avif')",
+      },
+    },
     colors: {
+      blue: "#3b769b",
+      dark: "#336079",
       transparent: "transparent",
       current: "currentColor",
       black: "#000000",
       white: "#ffffff",
       gray: colors.gray,
       indigo: colors.indigo,
-      neutral: colors.neutral,  // Used mainly for text color
-      yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
-      }, // Accent colors, used mainly for star color, heading and buttons
-      orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
-      }, // Primary colors, used mainly for links, buttons and svg icons
+      neutral: colors.neutral, // Used mainly for text color
+      yellow: colors.yellow,
+      orange: colors.orange, // Primary colors, used mainly for links, buttons and svg icons
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
+      sky: colors.sky, // Used for background color
+      amber: colors.amber, // Used for hover effect
     },
     extend: {},
+  },
+  flyonui: {
+    themes: ["corporate"],
   },
   plugins: [
     require("tailwindcss/nesting"),
     require("preline/plugin"),
     require("@tailwindcss/forms"),
+    require("flyonui"),
+    require("flyonui/plugin"),
   ],
 };
